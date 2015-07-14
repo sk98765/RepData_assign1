1dd636e98fcc4bd4184b57c394a9815ad1be86a7
charles chang <sk98765@gmail.com>
2015-07-13 22:50:33 -0400

    2nd version

--- assign1.R
+++ assign1.R
@@ -1,5 +1,6 @@
 library(data.table)
 
+setwd("d:/R/Reproducible/Assign1")
 dt <- fread("activity.csv")
 
 date.sum <- dt[,.(steps.sum = sum(steps)),by=.(date)]
@@ -9,3 +10,11 @@ hist(date.sum$steps.sum)
 mean.steps.sum <- mean(date.sum$steps.sum,na.rm=T)
 median.steps.sum <- median(date.sum$steps.sum, na.rm=T)
 
+# assign 0 to NA
+dt[is.na(dt$steps)] <- 0
+
+# group by interval
+avg.step <- dt[,.(inter.step.avg = mean(steps)),by=.(interval)]
+
+# 
+plot(inter.step.avg ~ interval, avg.step, type="l")
